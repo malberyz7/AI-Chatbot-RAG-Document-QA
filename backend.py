4b4397be22f47c77b43a6c7b3bedabfb15bd12a7
malberyz7 <alimzhan_meirambek@mail.ru>
2025-11-12 14:22:20 +0500

    work in progress

--- backend.py
+++ backend.py
@@ -1,6 +1,15 @@
-word = input()
-word_reversed = word[::-1]
-if word == word_reversed:
-    print("Yes")
-else:
-    print("No")
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Разрешаем запросы с фронтенда
+
+@app.route('/api/message', methods=['GET'])
+def get_message():
+    """API endpoint, возвращающий сообщение"""
+    return jsonify({
+        'message': 'Когда будем работать пацаны?'
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
