ddc64fc67b09610698f4822e0655606809849a6d
malberyz7 <alimzhan_meirambek@mail.ru>
2025-11-20 09:35:48 +0500

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, send_file
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    frontend_path = os.path.join(os.path.dirname(__file__), 'frontend.html')
+    return send_file(frontend_path)
+
+
+@app.route('/api/message', methods=['GET'])
+def get_message():
+    return jsonify({
+        'message': 'Когда будем работать пацаны?'
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
